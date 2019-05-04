03ad97c351ab362e69f840be4814cf902ed5d9d4
dlguswls <49232910+dlguswls@users.noreply.github.com>
2019-05-04 19:21:17 +0900

    Update naver.py

--- naver.py
+++ naver.py
@@ -1,5 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-
-req=requests.get('https://www.naver.com/')
-print(req.text)
+ 
+r = requests.get('https://www.naver.com/')
+soup = BeautifulSoup(r.text, 'html.parser')
+titles = soup.find_all('span',{'class':'ah_k'}) 
+num=0
+for x in titles:
+    num+=1
+    print("%d위:"%num,x.text)
