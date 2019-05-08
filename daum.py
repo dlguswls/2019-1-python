09cf190e0ddaad5fa4c87d5289e6c35f0f331f53
dlguswls <dlskadn77@naver.com>
2019-05-08 21:01:40 +0900

    inital commit

---
+++ daum.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+html = requests.get('https://www.daum.net/').text
+soup = BeautifulSoup(html, 'html.parser')
+
+title_list = soup.select('.hotissue_mini a[class*=link_issue]')
+ranking = soup.select('.list_mini span[class*=ir_wa]')
+
+for rank,title in zip(ranking , title_list):
+    r = ''.join(rank)
+    t = ''.join(title)
+
+    print("{}:{}".format(r, t))
