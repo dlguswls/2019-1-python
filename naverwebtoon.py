6949b26044f2d269a0dd0242231f5f022afe1cbe
dlguswls <dlskadn77@naver.com>
2019-05-14 15:30:28 +0900

    initial commit

--- naverwebtoon.py
+++ naverwebtoon.py
@@ -4,20 +4,30 @@ from bs4 import BeautifulSoup
 r = requests.get("https://comic.naver.com/webtoon/weekdayList.nhn?week=mon")
 soup = BeautifulSoup(r.text, 'html.parser')
 
-comics = soup.select('.list_area daily_img > ul >li')
+comics = soup.select('.list_area.daily_img > ul >li')
+episodes = soup.select('.tit_area.view')
 titlelist = []
 linklist = []
 scorelist = []
 episodelist = []
-
+epscorelist = []
+epdaylist = []
+i=0, k=0
 for x in comics:
     title = x.find('a').get('title')
-    for y in title:
-        ep_title = y.select('.title a[class*=nclk_v2]')
-        episodelist.append(ep_title)
     link = x.find('a').get('herf')
     score = x.find('.rating_type > span > strong')
     titlelist.append(title)
     linklist.append(link)
     scorelist.append(score)
-print(titlelist,linklist,scorelist,episodelist)
+    print(titlelist[i],linklist[i],scorelist[i]
+    i+=1
+    for y in episodes:
+        ep_title = y.find('h3').text
+        ep_score = y.find('span').get('strong')
+        ep_day = y.find('dl>dt>dd').get('date')
+        episodelist.append(ep_title)
+        episodelist.append(ep_score)
+        episodelist.append(ep_day)
+        print(episodelist[k],epscorelist[k],epdaylist[k])
+        k+=1
