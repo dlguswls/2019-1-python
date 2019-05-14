8ca18fc030462cdfb1632fe4819cfd3239cbb931
dlguswls <dlskadn77@naver.com>
2019-05-14 15:40:44 +0900

    initial commit

--- naverwebtoon.py
+++ naverwebtoon.py
@@ -8,26 +8,12 @@ comics = soup.select('.list_area.daily_img > ul >li')
 episodes = soup.select('.tit_area.view')
 titlelist = []
 linklist = []
-scorelist = []
-episodelist = []
-epscorelist = []
-epdaylist = []
-i=0, k=0
+
+i=0
 for x in comics:
     title = x.find('a').get('title')
-    link = x.find('a').get('herf')
-    score = x.find('.rating_type > span > strong')
+    link = x.find('a').get('href')
     titlelist.append(title)
     linklist.append(link)
-    scorelist.append(score)
-    print(titlelist[i],linklist[i],scorelist[i]
+    print(titlelist[i],linklist[i])
     i+=1
-    for y in episodes:
-        ep_title = y.find('h3').text
-        ep_score = y.find('span').get('strong')
-        ep_day = y.find('dl>dt>dd').get('date')
-        episodelist.append(ep_title)
-        episodelist.append(ep_score)
-        episodelist.append(ep_day)
-        print(episodelist[k],epscorelist[k],epdaylist[k])
-        k+=1
