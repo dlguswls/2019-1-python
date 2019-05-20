a0322d2760a32b175a14acef4fd73437fbb16b87
dlguswls <dlskadn77@naver.com>
2019-05-21 00:37:02 +0900

    initial commit

---
+++ Chrom.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get('http://google.com')
+search_box = driver.find_element_by_name("q")
+search_box.send_keys("개발새발 블로그")
+search_box.submit()
